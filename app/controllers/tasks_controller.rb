c7fbcc6d67221946f9732763005bb59caf88d3dd
Ito Satoshi <satoshi-ito@outlook.jp>
2016-09-04 11:36:58 +0900

    検索機能の向上

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -63,13 +63,17 @@ class TasksController < ApplicationController
 
   def search
     @search_value = params['search']['name']
+    @search_user = params['search']['user']
+    @search_category = params['search']['category']
     @tasks = Task.where("name like '%#{@search_value}%'")
     if @search_user.present?
       @tasks = @tasks.where(user_id: @search_user)
     end
+    if @search_category.present?
+      @tasks = @tasks.where(category_id: @search_category)
+    end
     render :index
   end
-
 
   private
     # Use callbacks to share common setup or constraints between actions.
